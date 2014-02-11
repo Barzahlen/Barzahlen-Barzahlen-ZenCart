9142543b3a10c08308718d9e2bb9c4246913d1de
Alexander Diebler <alexander.diebler@barzahlen.de>
2014-02-11 19:52:49 +0100

    Version 1.1.5
    - changed image urls to make use of https
    - updated cURL certificate bundle

--- tests/PHPUnit/ModuleBarzahlenTest.php
+++ tests/PHPUnit/ModuleBarzahlenTest.php
@@ -86,7 +86,7 @@ class ModuleBarzahlenTest extends PHPUnit_Framework_TestCase {
    */
   public function testSelection() {
 
-    $image = zen_image('http://cdn.barzahlen.de/images/barzahlen_logo.png');
+    $image = zen_image('https://cdn.barzahlen.de/images/barzahlen_logo.png');
     $title = MODULE_PAYMENT_BARZAHLEN_TEXT_TITLE;
     $description = MODULE_PAYMENT_BARZAHLEN_TEXT_FRONTEND_DESCRIPTION;
 
@@ -99,7 +99,7 @@ class ModuleBarzahlenTest extends PHPUnit_Framework_TestCase {
 
     for($i = 1; $i <= 10; $i++) {
       $count = str_pad($i,2,"0",STR_PAD_LEFT);
-      $description .= '<img src="http://cdn.barzahlen.de/images/barzahlen_partner_'.$count.'.png" alt="" style="vertical-align: middle; height: 25px;" />';
+      $description .= '<img src="https://cdn.barzahlen.de/images/barzahlen_partner_'.$count.'.png" alt="" style="vertical-align: middle; height: 25px;" />';
     }
 
     $expected = array('id' => 'barzahlen',
